8fe8dc3eec85fb3da78523fb002d091f4a521fb4
Sophie Gill <sophie-gill@hotmail.com>
2016-08-05 12:33:16 +0100

    adds city selection option

--- src/Interface.js
+++ src/Interface.js
@@ -36,11 +36,21 @@ $(document).ready(function() {
       updateTemperature();
       showPSM();
     });
-    
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  $('#current-temperature').text(data.main.temp);
-    })
 
+    function displayWeather(city) {
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+      var units = '&units=metric';
+      $.get(url + token + units, function(data) {
+        $('#current-temperature').text(data.main.temp);
+      });
+    }
 
+    displayWeather('London');
+
+    $('#current-city').change(function() {
+      var city = $('#current-city').val();
+      displayWeather(city);
+    });
 
 });
